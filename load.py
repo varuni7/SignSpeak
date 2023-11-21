e31ea477b3fa027148a7969378fa67ed46678e66
Tanisha Das <76507491+TanishaDas@users.noreply.github.com>
2023-11-21 17:56:45 +0530

    Create load.py
    Step 1, have taken 3 numbers with 1200 images each to to train, stores features and label in a pickle file called data

---
+++ load.py
@@ -0,0 +1,40 @@
+import mediapipe as mp
+import os
+import cv2
+import pickle
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+data = []
+labels = []
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+DATA_DIR = r'C:\Users\Satish Das\Downloads\GMM(Student)\data'
+width, height = 224, 224  # Specify the desired resolution
+
+for dir_ in os.listdir(DATA_DIR):
+    for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
+        data_aux = []
+        img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
+        
+        # Resize the image to a specific resolution
+        img_rgb = cv2.resize(img, (width, height))
+        
+        img_rgb = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2RGB)
+        results = hands.process(img_rgb)
+
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x)
+                    data_aux.append(y)
+            data.append(data_aux)
+            labels.append(dir_)
+            print(f"Processed image: {os.path.join(DATA_DIR, dir_, img_path)}")
+
+f = open('data.pickle', 'wb')
+pickle.dump({'data': data, 'labels': labels}, f)
+f.close()
